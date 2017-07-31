aeff150a480969e5bf9d3c2a1dda937f978ce451
luisnoejasso <luisnoejasso@hotmail.com>
2017-07-31 12:21:54 -0500

    init roit

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,9 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
- 
+
 export const Tasks = new Mongo.Collection('tasks');
 if (Meteor.isServer) {
+
+  Router.route('/auth', function () {
+    var name = this.params;
+    var data = this.request.body;
+
+    this.response.writeHead(404, {
+      'Content-Type': 'application/json'
+    });
+    this.response.end("404-Sin autorización. " + (data));
+
+
+
+  }, { where: 'server' });
   // This code only runs on the server
   Meteor.publish('tasks', function tasksPublication() {
     return Tasks.find();
@@ -12,27 +25,27 @@ if (Meteor.isServer) {
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
- 
+
     // Make sure the user is logged in before inserting a task
     //if (! Meteor.userId()) {
-      //throw new Meteor.Error('not-authorized');
-   // }
- 
+    //throw new Meteor.Error('not-authorized');
+    // }
+
     Tasks.insert({
       text,
       createdAt: new Date(),
-     
+
     });
   },
   'tasks.remove'(taskId) {
     check(taskId, String);
- 
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
     check(taskId, String);
     check(setChecked, Boolean);
- 
+
     Tasks.update(taskId, { $set: { checked: setChecked } });
   },
 });
